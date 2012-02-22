14b070e39e1b1aaa7127d2addfbceaf8bc81c9f6
Steven Haryanto <stevenharyanto@gmail.com>
2012-02-22 05:04:22 +0000

    Import of SHARYANTO/Riap-1.1.7 from CPAN.
    gitpan-cpan-distribution: Riap
    gitpan-cpan-version:      1.1.7
    gitpan-cpan-path:         SHARYANTO/Riap-1.1.7.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Riap.pm
+++ lib/Riap.pm
@@ -1,6 +1,6 @@
 package Riap;
 
-our $VERSION = '1.1.6'; # VERSION
+our $VERSION = '1.1.7'; # VERSION
 
 1;
 # ABSTRACT: Rinci access protocol
@@ -14,7 +14,7 @@ Riap - Rinci access protocol
 
 =head1 VERSION
 
-version 1.1.6
+version 1.1.7
 
 =head1 AUTHOR
 
