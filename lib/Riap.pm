ac083466a29817be8cbb9155de967d641abd5b04
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-27 05:02:59 +0000

    Import of SHARYANTO/Riap-1.1.20 from CPAN.
    gitpan-cpan-distribution: Riap
    gitpan-cpan-version:      1.1.20
    gitpan-cpan-path:         SHARYANTO/Riap-1.1.20.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Riap.pm
+++ lib/Riap.pm
@@ -1,12 +1,15 @@
 package Riap;
 
-our $VERSION = '1.1.19'; # VERSION
+our $VERSION = '1.1.20'; # VERSION
 
 1;
 # ABSTRACT: Rinci access protocol
 
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -14,7 +17,23 @@ Riap - Rinci access protocol
 
 =head1 VERSION
 
-version 1.1.19
+version 1.1.20
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Riap>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Riap>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Riap>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -22,10 +41,9 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
