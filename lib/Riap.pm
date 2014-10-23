830e978c8fcd8e31440c74104f76e97ee54845b5
perlancar <perlancar@gmail.com>
2014-10-23 07:17:14 +0000

    Import of PERLANCAR/Riap-1.2.1 from CPAN.
    gitpan-cpan-distribution: Riap
    gitpan-cpan-version:      1.2.1
    gitpan-cpan-path:         PERLANCAR/Riap-1.2.1.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Riap.pm
+++ lib/Riap.pm
@@ -1,7 +1,7 @@
 package Riap;
 
 our $DATE = '2014-10-23'; # DATE
-our $VERSION = '1.2.0'; # VERSION
+our $VERSION = '1.2.1'; # VERSION
 
 1;
 # ABSTRACT: Rinci access protocol
@@ -18,7 +18,7 @@ Riap - Rinci access protocol
 
 =head1 VERSION
 
-This document describes version 1.2.0 of Riap (from Perl distribution Riap), released on 2014-10-23.
+This document describes version 1.2.1 of Riap (from Perl distribution Riap), released on 2014-10-23.
 
 =head1 HOMEPAGE
 
